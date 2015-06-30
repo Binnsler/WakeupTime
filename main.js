2f0ad46a6dcf635c51fb71e499f5763f62a9208f
Jamie Binns <jamie.m.binns@gmail.com>
2015-06-30 15:49:07 -0600

    Added in little counter, which could be added to make the clock function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).on('ready', function(){
 	var outerShell = $('<div class="outerShell"></div>');
 	var innerShell = $('<div class="innerShell"></div>');
+	var leftAmPm = $('<div class="leftAmPm"></div>')
 	var pmText = $('<p class="pmText">PM</p>');
 	var autoText = $('<p class="autoText">Auto</p>');
 	var amLabelFreq = $('<p class="amLabelFreq">AM 0 70 90 110 140 170</p>');
@@ -11,8 +12,9 @@ $(document).on('ready', function(){
 
 	$('.container').append(outerShell);
 	outerShell.append(innerShell);
-	innerShell.append(pmText);
-	innerShell.append(autoText);
+	innerShell.append(leftAmPm)
+	leftAmPm.append(pmText);
+	leftAmPm.append(autoText);
 	innerShell.append(clockScreen);
 	innerShell.append(amLabelFreq);
 	innerShell.append(pmLabelFreq);
@@ -21,3 +23,9 @@ $(document).on('ready', function(){
 
 
 });
+
+setInterval(function(){
+	var now = new Date();
+	
+	console.log(now.toLocaleTimeString());
+}, 1000);
